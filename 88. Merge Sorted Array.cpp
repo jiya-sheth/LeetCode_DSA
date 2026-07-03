39de7f311ee5040872503fef2424cd338b75ed97
Jiya Sheth <161239014+jiya-sheth@users.noreply.github.com>
2026-07-03 23:53:42 +0530

    Create 88. Merge Sorted Array.cpp

---
+++ 88. Merge Sorted Array.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int len = n + m;
+        int gap = (len / 2) + (len % 2);
+
+        while(gap > 0) 
+        {
+            int left = 0;
+            int right = left + gap;
+            while(right < len) 
+            {
+                if(left < m && right >= m) 
+                {
+                    swapIfGreater(nums1, nums2, left, right - m);
+                }
+                else if(left >= m) 
+                {
+                    swapIfGreater(nums2, nums2, left - m, right - m);
+                }
+                else 
+                {
+                    swapIfGreater(nums1, nums1, left, right);
+                }
+                left++, right++;
+            }
+            if(gap == 1) break;
+            gap = (gap / 2) + (gap % 2);
+        }
+        for(int i = m; i < m + n; i++) 
+        {
+            nums1[i] = nums2[i - m];
+        }
+    }
+private:
+    void swapIfGreater(vector<int>& arr1, vector<int>& arr2, int idx1, int idx2) 
+    {
+        if(arr1[idx1] > arr2[idx2]) 
+        {
+            swap(arr1[idx1], arr2[idx2]);
+        }
+    }
+};
